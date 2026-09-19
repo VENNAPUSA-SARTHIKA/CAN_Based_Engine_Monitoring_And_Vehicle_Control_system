c79606cfc5151fe9167b0af577400c65d74f04d8
VENNAPUSA_SARTHIKA <vennapusasarthika@gmail.com>
2026-09-19 13:00:30 +0530

    Add files via upload

---
+++ MAIN_NODE.c
@@ -0,0 +1,376 @@
+#include <LPC21xx.h>
+
+#include "CAN.h"
+#include "CAN_defines.h"
+#include "LCD.h"
+#include "EINT.h"
+#include "delays.h"
+#include "pro_funcs.h"
+
+/*=========================================================
+ * Global Variables
+ *=========================================================*/
+
+volatile MODE_T CurrentMode = DASHBOARD_MODE;
+
+volatile u8 ReverseDistance = 0;
+
+volatile MODE_T PreviousMode;
+
+volatile u8 WindowStopPending = 0;
+volatile u8 ReverseStopPending = 0;
+
+
+volatile u16 WindowTimeout=0;
+volatile u16 ReverseTimeout=0;
+volatile u8 WindowAlive=0;
+/*=========================================================
+ * CAN Frame Structures
+ *=========================================================*/
+
+struct CAN_Frame TX_frame1;
+struct CAN_Frame TX_frame2;
+struct CAN_Frame Rx_frame;
+
+/*=========================================================
+ * MAIN FUNCTION
+ *=========================================================*/
+
+int main(void)
+{
+	
+    /*-----------------------------------------------------
+     * LCD Initialization
+     *----------------------------------------------------*/
+    init_LCD();
+
+    cmd_LCD(0x01);
+    cmd_LCD(0x80);
+    str_LCD("LCD OK");
+    delay_ms(500);
+
+    /*-----------------------------------------------------
+     * CAN Initialization
+     *----------------------------------------------------*/
+    Init_CAN1();
+
+    cmd_LCD(0x01);
+    cmd_LCD(0x80);
+    str_LCD("CAN OK");
+    delay_ms(500);
+
+    /*-----------------------------------------------------
+     * Interrupt Initialization
+     *----------------------------------------------------*/
+    Interrupt_Init();
+
+    cmd_LCD(0x01);
+    cmd_LCD(0x80);
+    str_LCD("EINT OK");
+    delay_ms(500);
+
+    /*-----------------------------------------------------
+     * Buzzer Output
+     *----------------------------------------------------*/
+    IODIR0 |= (1U << 21);
+
+    /* Active HIGH buzzer OFF */
+    IOSET0 = (1U << 21);
+
+    /*-----------------------------------------------------
+     * Window Control CAN Frame
+     *----------------------------------------------------*/
+    TX_frame1.ID      = CAN_ID_WINDOW_CTRL;
+    TX_frame1.vbf.RTR = 0;
+    TX_frame1.vbf.DLC = 1;
+		TX_frame1.Data1 = 0;
+		TX_frame1.Data2 = 0;
+    /*-----------------------------------------------------
+     * Reverse Alert CAN Frame
+     *----------------------------------------------------*/
+    TX_frame2.ID      = CAN_ID_REVERSE_ENABLE;
+    TX_frame2.vbf.RTR = 0;
+    TX_frame2.vbf.DLC = 1;
+		TX_frame2.Data1 = 0;
+		TX_frame2.Data2 = 0;
+
+    /*-----------------------------------------------------
+     * Startup Screen
+     *----------------------------------------------------*/
+		vehicle();
+
+		Return_To_Dashboard();
+
+		Vehicle_Dashboard();
+
+		Dashboard_Update();
+
+		PreviousMode = DASHBOARD_MODE;
+    /*=====================================================
+     * Main Loop
+     *====================================================*/
+
+    while(1)
+    {
+				
+        switch(CurrentMode)
+        {
+            /*=================================================
+             * WINDOW UP MODE
+             *================================================*/
+							case WINDOW_UP_MODE:
+
+										if(PreviousMode != WINDOW_UP_MODE)
+										{
+												WindowTimeout=0;
+												
+												TX_frame1.Data1 = WINDOW_UP_START_CMD;
+												TX_frame1.Data2 = 0;
+
+												CAN1_Tx(TX_frame1);
+												Window_Up_Mode();
+												WindowStopPending = 1;
+												PreviousMode = WINDOW_UP_MODE;
+										}
+
+									
+										if(CAN1_Rx(&Rx_frame))
+										{		
+											if(Rx_frame.ID == CAN_ID_WINDOW_STATUS)
+											{
+												WindowTimeout=0;
+												Window_Up_Animation();
+												//WindowNodeAlive=1;
+											}
+										}
+										else
+										{
+											WindowTimeout++;
+										}
+										/*WindowTimeout++;
+										cmd_LCD(0x80);
+										u32_LCD(WindowTimeout);*/
+
+										if(WindowTimeout>5)
+										{
+											show_window_error();
+											
+											PreviousMode=DASHBOARD_MODE;
+											CurrentMode=DASHBOARD_MODE;
+											WindowTimeout=0;
+											WindowStopPending = 0;
+											break;
+										}
+
+										
+										Dashboard_Update();
+										
+										delay_ms(30);
+
+										break;
+            /*=================================================
+             * WINDOW DOWN MODE
+             *================================================*/
+						
+							case WINDOW_DOWN_MODE:
+
+									if(PreviousMode != WINDOW_DOWN_MODE)
+										{
+												WindowTimeout=0;
+												
+												TX_frame1.Data1 = WINDOW_DOWN_START_CMD;
+												TX_frame1.Data2 = 0;
+
+												CAN1_Tx(TX_frame1);
+												Window_Down_Mode();
+												WindowStopPending = 1;
+												PreviousMode = WINDOW_DOWN_MODE;
+										}
+
+									
+										if(CAN1_Rx(&Rx_frame))
+										{		
+											if(Rx_frame.ID == CAN_ID_WINDOW_STATUS)
+											{
+												WindowTimeout=0;
+												Window_Down_Animation();
+												//WindowNodeAlive=1;
+											}
+										}
+										else
+										{
+											WindowTimeout++;
+										}
+										if(WindowTimeout>5)
+										{
+											show_window_error();
+											
+											PreviousMode=DASHBOARD_MODE;
+											CurrentMode=DASHBOARD_MODE;
+											WindowTimeout=0;
+											WindowStopPending = 0;
+											break;
+										}
+
+										
+										Dashboard_Update();
+										
+										delay_ms(30);
+
+										break;           
+						/*=================================================
+						* REVERSE MODE
+						*================================================*/
+						case REVERSE_MODE:
+
+								if(PreviousMode != REVERSE_MODE)
+								{
+										Reverse_Mode_Screen();
+
+										TX_frame2.Data1 = REVERSE_ON;
+										TX_frame2.Data2 = 0;
+
+										CAN1_Tx(TX_frame2);
+
+										
+
+										ReverseStopPending = 1;
+
+										PreviousMode = REVERSE_MODE;
+								}
+
+								if(CAN1_Rx(&Rx_frame))
+								{
+										if(Rx_frame.ID == CAN_ID_DISTANCE)
+										{
+												//ReverseNodeAlive=1;
+												ReverseTimeout=0;
+												ReverseDistance = Rx_frame.Data1;
+
+												cmd_LCD(0xC0);
+												str_LCD("DIST : ");
+
+												u32_LCD(ReverseDistance);
+
+												str_LCD(" CM   ");
+
+												if(ReverseDistance > SAFE_ZONE_LIMIT)
+												{
+														cmd_LCD(0x94);
+														str_LCD("ZONE : SAFE      ");
+
+														cmd_LCD(0xD4);
+														str_LCD("PATH CLEAR       ");
+												}
+												else if(ReverseDistance > WARNING_ZONE_LIMIT)
+												{
+														cmd_LCD(0x94);
+														str_LCD("ZONE : WARNING   ");
+
+														cmd_LCD(0xD4);
+														str_LCD("OBJECT NEARBY    ");
+												}
+												else if(ReverseDistance > DANGER_ZONE_LIMIT)
+												{
+														cmd_LCD(0x94);
+														str_LCD("ZONE : DANGER    ");
+
+														cmd_LCD(0xD4);
+														str_LCD("SLOW DOWN        ");
+												}
+												else
+												{
+														Buzzer_Beep();
+
+														cmd_LCD(0x94);
+														str_LCD("ZONE : CRITICAL  ");
+
+														cmd_LCD(0xD4);
+														str_LCD("STOP VEHICLE     ");
+												}
+										}
+								}
+								else
+								{
+									ReverseTimeout++;
+								}
+								if(ReverseTimeout>50)
+								{
+									show_reverse_error();
+
+									ReverseTimeout=0;
+							//		ReverseNodeAlive=0;
+
+									CurrentMode=DASHBOARD_MODE;
+									PreviousMode=DASHBOARD_MODE;
+									ReverseStopPending=0;
+									break;
+								}
+
+
+								delay_ms(30);
+
+								break;
+
+            /*=================================================
+             * DASHBOARD MODE
+             *================================================*/
+            case DASHBOARD_MODE:
+							WindowTimeout=0;
+							ReverseTimeout=0;
+
+							if(WindowStopPending)
+							{
+									TX_frame1.Data1 = WINDOW_STOP_CMD;
+									TX_frame1.Data2 = 0;
+
+									CAN1_Tx(TX_frame1);
+
+									WindowStopPending = 0;
+							}
+
+							if(ReverseStopPending)
+							{
+									TX_frame2.Data1 = REVERSE_OFF;
+									TX_frame2.Data2 = 0;
+
+									CAN1_Tx(TX_frame2);
+
+									ReverseStopPending = 0;
+							}
+							if(CAN1_Rx(&Rx_frame))
+							{
+								switch(Rx_frame.ID)
+								{
+									case CAN_ID_WINDOW_STATUS:
+										WindowTimeout=0;
+										break;
+
+									case CAN_ID_DISTANCE:
+										ReverseTimeout=0;
+										break;
+
+								}
+							 }
+
+							if(PreviousMode != DASHBOARD_MODE)
+							{
+									Vehicle_Dashboard();
+
+									PreviousMode = DASHBOARD_MODE;
+							}
+
+							Dashboard_Update();
+
+							delay_ms(10);
+
+							break;
+						
+					default:
+
+						CurrentMode = DASHBOARD_MODE;
+
+						break;
+        }
+    }
+}
---
+++ project_functions.c
@@ -0,0 +1,466 @@
+#include <LPC21xx.h>
+
+
+#include "LCD.h"
+
+#include "DS18B20.h"
+
+#include "delays.h"
+
+#include "types.h"
+
+#include "pro_funcs.h"
+
+#include "CAN.h"
+
+#include "CAN_defines.h"
+
+
+/*=========================================================
+
+                ENGINE TEMPERATURE LIMITS
+
+=========================================================*/
+
+
+#define TEMP_NORMAL_MAX      40
+
+#define TEMP_WARM_MAX        70
+
+#define TEMP_CRITICAL_MAX    90
+
+
+/*=========================================================
+
+                GLOBAL VARIABLES
+
+=========================================================*/
+
+
+/*=========================================================
+
+                DISPLAY STATES
+
+=========================================================*/
+
+typedef enum
+{
+    DISPLAY_STARTUP = 0,
+    DISPLAY_DASHBOARD,
+    DISPLAY_WINDOW_UP,
+    DISPLAY_WINDOW_DOWN,
+    DISPLAY_REVERSE,
+    DISPLAY_WARNING
+}DISPLAY_STATE_T;
+
+static unsigned char WindowLevel = 0;
+static int EngineTemp;
+static DISPLAY_STATE_T DisplayState = DISPLAY_STARTUP;
+
+f32 temp;
+s32 tVal;
+u16 intPart,fracPart;
+
+
+/*=========================================================
+
+                DISPLAY TEMPERATURE
+
+=========================================================*/
+
+void Read_Temperature(void)
+{
+		s8 sign=1;
+    temp = (ds18b20_read_temperature());
+		tVal=temp*10000;
+	
+    if(tVal < 0)
+    {
+        sign=-1;
+        tVal = -tVal;
+    }
+
+    intPart = tVal / 10000;
+    fracPart = tVal % 10000;
+
+    EngineTemp = (sign < 0) ? -intPart : intPart;
+}
+
+void Display_Temperature(void)
+ {
+ 	if(EngineTemp < 0)
+     {
+         char_LCD('-');
+ 	}
+		u32_LCD(intPart);
+    char_LCD('.');
+    char_LCD((fracPart/1000) + '0');
+   char_LCD(((fracPart/100) % 10) + '0');
+
+    char_LCD(0xDF);
+    char_LCD('C');
+}
+
+/*=========================================================
+
+                ENGINE STATUS
+
+=========================================================*/
+
+void Display_Engine_Status(void)
+{
+    if(EngineTemp < TEMP_NORMAL_MAX)
+    {
+        str_LCD("NORMAL     ");
+    }
+    else if(EngineTemp < TEMP_WARM_MAX)
+   {
+        str_LCD("WARMING    ");
+    }
+    else if(EngineTemp < TEMP_CRITICAL_MAX)
+    {
+        str_LCD("HIGH TEMP  ");
+    }
+    else
+    {
+        str_LCD("CRITICAL   ");
+    }
+}
+
+/*=========================================================
+
+                OVERHEAT WARNING
+
+=========================================================*/
+
+void Overheat_Warning(void)
+{
+    if(DisplayState == DISPLAY_WARNING)
+        return;
+    cmd_LCD(0x01);
+
+    cmd_LCD(0x80);
+    str_LCD("!!!!! WARNING !!!!!!");
+    cmd_LCD(0xC0);
+	str_LCD("ENGINE OVERHEAT     ");
+
+     cmd_LCD(0x94);
+	 str_LCD("STOP VEHICLE NOW    ");
+
+    cmd_LCD(0xD4);
+    str_LCD("CHECK COOLING SYS   ");
+
+    DisplayState = DISPLAY_WARNING;
+}
+
+/*=========================================================
+
+                STARTUP SCREEN
+
+=========================================================*/
+
+void Startup_Screen(void)
+{
+    cmd_LCD(0x01);
+
+    cmd_LCD(0x80);
+    str_LCD("..CAN BASED ENGINE..");
+
+    cmd_LCD(0xC0);
+    str_LCD("MONITORING SYSTEM   ");
+
+    cmd_LCD(0x94);
+    str_LCD("VEHICLE CONTROL     ");
+
+    cmd_LCD(0xD4);
+    str_LCD("INITIALIZING........");
+
+   delay_s(2);
+    cmd_LCD(0x01);
+}
+
+
+/*=========================================================
+
+                DASHBOARD
+
+=========================================================*/
+
+
+void Vehicle_Dashboard(void)
+{
+	    if(DisplayState != DISPLAY_DASHBOARD)
+			{
+				cmd_LCD(0x01);
+
+				cmd_LCD(0x80);
+				str_LCD("*****ENGINE SYS*****");
+
+			cmd_LCD(0xC0);
+			str_LCD("ENG TEMP:  ");
+
+			cmd_LCD(0x94);
+			str_LCD("MODE    : FORWARD   ");
+
+			cmd_LCD(0xD4);
+			str_LCD("STATUS  : ");
+			DisplayState = DISPLAY_DASHBOARD;
+		}
+}
+
+void Dashboard_Update(void)
+{
+	    /* Always read latest temperature */
+	    Read_Temperature();
+
+	     /* Warning screen currently active */
+	    if(DisplayState == DISPLAY_WARNING)
+	    {
+	        if(EngineTemp < TEMP_CRITICAL_MAX)
+	        {
+	            Return_To_Dashboard();
+	   						Vehicle_Dashboard();
+
+						cmd_LCD(0xCA);
+						Display_Temperature();
+
+						cmd_LCD(0xDE);
+						str_LCD("          ");
+
+						cmd_LCD(0xDE);
+						Display_Engine_Status();
+						return;
+        }
+        else
+        {
+            return;
+        }
+    }
+    /* Temperature became critical */
+    if(EngineTemp >= TEMP_CRITICAL_MAX)
+    {
+        Overheat_Warning();
+        return;
+     }
+
+    /* Update only temperature always */
+		cmd_LCD(0xCA);
+		Display_Temperature();
+		str_LCD("   ");
+
+		/* Update engine status only in Dashboard */
+		if(DisplayState == DISPLAY_DASHBOARD)
+		{
+			cmd_LCD(0xDE);
+			str_LCD("          ");
+
+			cmd_LCD(0xDE);
+			Display_Engine_Status();
+		}
+}
+/*=========================================================
+
+                WINDOW UP ANIMATION
+
+=========================================================*/
+
+
+void Window_Up_Animation(void)
+{
+    unsigned int i;
+    if(WindowLevel < 8)
+        WindowLevel++;
+
+    cmd_LCD(0xD4);
+    str_LCD("LEVEL : [");
+
+    for(i=0;i<WindowLevel;i++)
+    {
+        char_LCD(0xFF);
+    }
+
+    for(i=WindowLevel;i<8;i++)
+    {
+       char_LCD(' ');
+    }
+
+    char_LCD(']');
+}
+
+/*=========================================================
+
+                WINDOW DOWN ANIMATION
+
+=========================================================*/
+
+void Window_Down_Animation(void)
+{
+    unsigned int i;
+
+    if(WindowLevel > 0)
+        WindowLevel--;
+    cmd_LCD(0xD4);
+
+   str_LCD("LEVEL : [");
+
+    for(i=0;i<WindowLevel;i++)
+    {
+        char_LCD(0xFF);
+    }
+
+    for(i=WindowLevel;i<8;i++)
+     {
+         char_LCD(' ');
+     }
+     char_LCD(']');
+}
+
+/*=========================================================
+
+                WINDOW OPEN
+
+=========================================================*/
+
+void Window_Up_Mode(void)
+{
+    if(DisplayState != DISPLAY_WINDOW_UP)
+    {
+
+        cmd_LCD(0x94);			
+        str_LCD("W MODE:OPEN  ST:MOVE");
+
+        DisplayState = DISPLAY_WINDOW_UP;
+    }
+}
+
+/*=========================================================
+
+                WINDOW CLOSE
+
+=========================================================*/
+
+
+void Window_Down_Mode(void)
+{
+    if(DisplayState != DISPLAY_WINDOW_DOWN)
+    {
+        cmd_LCD(0x94);
+         str_LCD("W MODE:CLOSE ST:MOVE");
+
+        DisplayState = DISPLAY_WINDOW_DOWN;
+    }
+}
+/*=========================================================
+
+                START VEHICLE
+
+=========================================================*/
+void vehicle(void)
+{
+    Startup_Screen();
+	  DisplayState = DISPLAY_STARTUP;
+}
+/*=========================================================
+
+                REVERSE SCREEN
+
+=========================================================*/
+
+
+void Reverse_Mode_Screen(void)
+{
+	  if(DisplayState == DISPLAY_REVERSE)
+ 		return;
+
+    cmd_LCD(0x01);
+        cmd_LCD(0x80);
+      str_LCD("****REVERSE MODE****");
+        cmd_LCD(0xC0);
+ 
+        str_LCD("MODE : ACTIVE       ");
+            cmd_LCD(0x94);
+        str_LCD("DIST : WAIT...      ");
+
+		   cmd_LCD(0xD4);
+	    str_LCD("STATUS : READY      ");
+		
+		DisplayState = DISPLAY_REVERSE;
+}
+/*=========================================================
+
+                ACTIVE HIGH BUZZER
+
+=========================================================*/
+
+void Buzzer_Beep(void)
+{
+    IOCLR0 = (1<<21);
+
+    delay_ms(300);
+
+    IOSET0 = (1<<21);
+}
+
+/*=========================================================
+
+                RETURN TO DASHBOARD
+
+=========================================================*/
+void Return_To_Dashboard(void)
+{
+    DisplayState = DISPLAY_STARTUP;
+}
+
+ //Window Node error
+
+void show_window_error(void)
+{
+
+	cmd_LCD(0x01);
+	cmd_LCD(0x80);
+	str_LCD("*****  ERROR   *****");
+
+	cmd_LCD(0xc0);
+	str_LCD("..WINDOW NODE FAIL..");
+
+	cmd_LCD(0x94);
+	str_LCD("..CHECK CAN WIRING..");
+
+	cmd_LCD(0xD4);
+	str_LCD("..NODE  NOT  FOUND..");
+
+	delay_ms(1500);
+
+	Return_To_Dashboard();
+	Vehicle_Dashboard();
+	Dashboard_Update();
+}
+
+//Reverse Node error
+
+void show_reverse_error(void)
+{
+
+	cmd_LCD(0x01);
+
+	cmd_LCD(0x80);
+	str_LCD("*****  ERROR   *****");
+
+	cmd_LCD(0xc0);
+	str_LCD(".REVERSE NODE  FAIL.");
+
+	cmd_LCD(0x94);
+	str_LCD("..CHECK CAN WIRING..");
+
+	cmd_LCD(0xD4);
+	str_LCD("..NODE  NOT  FOUND..");
+
+	delay_ms(1500);
+	Return_To_Dashboard();
+	Vehicle_Dashboard();
+	Dashboard_Update();
+}
+
+
+
+
